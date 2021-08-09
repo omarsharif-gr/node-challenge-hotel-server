fe2c58311ae1d6421f4b18ac579dd477a7548359
omarsharifgr <omarsharif.os@yahoo.com>
2021-08-09 01:18:35 +0100

    Completed Level 1
    Installed Dependencies and completed level 1

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const moment = require("moment")
+
 const app = express();
 
 app.use(express.json());
@@ -8,13 +10,51 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+const { request } = require("http");
+const { response } = require("express");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+// Read all bookings
+app.get("/bookings", function (request, response) {
+  response.json(bookings);
+});
+
+
+// Read one booking as specified by an ID
+app.get("/bookings/:bookingId", function (request, response) {
+  const findBooking = bookings.filter((booking) => {
+    return booking.id === parseInt(request.params.bookingId);
+  });
+  if (findBooking.length === 0) {
+    response
+      .status(404)
+      .send(`There is no booking with the id of ${request.params.bookingId}`);
+  } else {
+    response.json(findBooking);
+  }
+});
+
+// Delete one booking:
+app.delete("/bookings/:bookingId", (req, res)=> {
+  const originalLengthOfBookings = bookings.length;
+  bookings = bookings.filter((booking) => {
+    return booking.id != request.params.bookingId;
+  });
+  if (originalLengthOfBookings === bookings.length) {
+    response.status(404).send(`${request.params.bookingId} is not in our database.`)
+  } else {
+    response.json(bookings);
+  }
+  })
+
+
+// Add one booking
+
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
